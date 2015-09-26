c876f94f2b4530cc1879dbf186743c3b276df2cb
vsatanasov <vsatanasov@gmail.com>
2015-09-26 20:02:11 +0300

    more html

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,4 +5,35 @@ $(function(){
     var registerForm = new RegisterForm({
         form: $('#register-form')
     })
+
+    new App($('nav'));
 });
+
+function App($menu) {
+    this.$menu = $menu;
+    this.init();
+    this.onMenuClick({});
+}
+
+App.prototype.init = function () {
+    var _this = this;
+    this.$menu.find('a').on('click', function(e) {
+        e.preventDefault();
+        _this.onMenuClick(this);
+    });
+}
+
+App.prototype.onMenuClick = function (link) {
+    $('.site-panel').hide('fast', function() {
+        var $el = $($(link).attr('href'));
+
+        if ($el.length) {
+            $el.show();
+
+        } else {
+            $('#index').show();
+        }
+    });
+
+
+}
